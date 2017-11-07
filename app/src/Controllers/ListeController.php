3d3e9c3eeb3824e0c6df2c1ed1de4a4588e4e481
thibaudG <thibaud.grepin@gmail.com>
2017-11-07 11:11:08 +0100

    ajout controller function token

---
+++ app/src/Controllers/ListeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use App\Models\Liste;
+
+final class ListeController
+{
+
+    /*
+     * Function qui genere un token, l'ajoute a la base et le renvoi
+     * destiné au créateur, qui pourra le partager
+     * idliste l'id de la liste actuelle
+     */
+    public function generateToken(idliste){
+        $token==bin2hex(random_bytes(16));
+        $liste=Liste::where("id","=",$idliste)->first();
+        $liste->save();
+        $liste->token=$token;
+        return $token;
+    }
+}
