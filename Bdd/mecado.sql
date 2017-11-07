844a366efc91afae652d2496260fa9070090f331
Orionc <Nicolas.jcqm@gmail.com>
2017-11-07 10:03:58 +0100

    add Bdd

---
+++ Bdd/mecado.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 07 Novembre 2017 à 09:01
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `mecado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(10) NOT NULL,
+  `reserverName` varchar(50) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `idItem` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(10) NOT NULL,
+  `senderName` varchar(50) NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `idList` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creator`
+--
+
+CREATE TABLE `creator` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `mail` varchar(60) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(10) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `price` float NOT NULL,
+  `url` varchar(200) NOT NULL,
+  `picture` varchar(50) NOT NULL,
+  `idGroup` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `list`
+--
+
+CREATE TABLE `list` (
+  `id` int(10) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `validityDate` date NOT NULL,
+  `token` varchar(20) NOT NULL,
+  `isRecipient` tinyint(1) NOT NULL,
+  `idCreator` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participant`
+--
+
+CREATE TABLE `participant` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `amountGiven` float NOT NULL,
+  `idPot` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pot`
+--
+
+CREATE TABLE `pot` (
+  `id` int(10) NOT NULL,
+  `amount` float NOT NULL,
+  `currentSum` float NOT NULL,
+  `idItem` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_booking_idItem` (`idItem`);
+
+--
+-- Index pour la table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_comment_idlist` (`idList`);
+
+--
+-- Index pour la table `creator`
+--
+ALTER TABLE `creator`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_list_idcreator` (`idCreator`);
+
+--
+-- Index pour la table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_participant_idPot` (`idPot`);
+
+--
+-- Index pour la table `pot`
+--
+ALTER TABLE `pot`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pot_idItem` (`idItem`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
