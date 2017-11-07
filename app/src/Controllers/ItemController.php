a185d5fced5c37d0f212c435e1635d3a71fc6d16
Marquant Lucas <lucas.marquant2@etu.univ-lorraine.fr>
2017-11-07 16:15:39 +0100

    Ajout de la methode addItem qui ajoute un item dans la base

--- app/src/Controllers/ItemController.php
+++ app/src/Controllers/ItemController.php
@@ -20,6 +20,25 @@ final class ItemController
     }
 
     public function addItem($title, $desc, $price, $url , $picture){
+        $Item = new \app\Models\Item();
+        
+        $Item->title = $title;
+        $Item->description = $desc;
+        $Item->price = $price;
+        $Item->url = $url;
+        $Item->idGroup = null;
+        
+        if (isset($_FILES['fileToUpload']) AND $_FILES['fileToUpload'] ['error'] == 0){
+            $nomdate = date('o').'-'.date("m").'-'.date('d').'-'.date('H').'-'.date('i').'-'.date('s');
+            $ext = pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES['fileToUpload'] ['tmp_name'], 'img/' .$nomdate.'.'.$ext);
+            $Item->picture; = $nomdate.'.'.$ext;
+            
+        }else{
+            echo "Sorry, there was an error uploading your file.";
+        }
+        
+        $Item->save();
         
     }
 }
