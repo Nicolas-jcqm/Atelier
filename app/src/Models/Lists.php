d63be968a2b1002a130f98cce0e515d9e425d232
Orionc <Nicolas.jcqm@gmail.com>
2017-11-07 11:07:19 +0100

    add Model Lists

---
+++ app/src/Models/Lists.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas
+ * Date: 07/11/2017
+ * Time: 11:01
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lists extends Model
+{
+
+    protected $table = 'list';
+    protected $primaryKey = 'id';
+    public $timestamps = 'false';
+
+}
