ac765f0c7fcd44377a27e323f1df93dfbcba0cbf
Matmat Myriam <matmat.myriam@gmail.com>
2017-11-07 22:49:23 +0100

    add middleware

--- app/route.php
+++ app/route.php
@@ -6,14 +6,14 @@
 
 $app->get('/', 'App\Controllers\HomeController:dispatch')->setName('homepage');
 
-$app->get('/signup', 'App\Controllers\UserController:signup')->setName('signup');
+$app->get('/signup', 'App\Controllers\UserController:signup')->setName('signup')->add($middleware_need_no_co);
 
 $app->post('/signup', 'App\Controllers\UserController:validation_signup');
 
-$app->get('/signin', 'App\Controllers\UserController:signin')->setName('signin');
+$app->get('/signin', 'App\Controllers\UserController:signin')->setName('signin')->add($middleware_need_no_co);
 
 $app->post('/signin', 'App\Controllers\UserController:validation_signin');
 
 $app->get('/lists', 'App\Controllers\ListeController:displayLists')->setName('lists');
 
-$app->get('/homeCo','App\Controllers\UserController:homeCo')->setName('homeCo');
+$app->get('/homeCo','App\Controllers\UserController:homeCo')->setName('homeCo')->add($middleware_need_co);
