9dde24f51d4818b93d082ec128caf859f3722098
Matmat Myriam <matmat.myriam@gmail.com>
2017-11-08 11:58:28 +0100

    add creationList and add on base

--- app/route.php
+++ app/route.php
@@ -6,6 +6,8 @@
 
 $app->get('/', 'App\Controllers\HomeController:dispatch')->setName('homepage')->add($middleware_need_no_co);
 
+//authentification
+
 $app->get('/signup', 'App\Controllers\UserController:signup')->setName('signup')->add($middleware_need_no_co);
 
 $app->post('/signup', 'App\Controllers\UserController:validation_signup');
@@ -13,6 +15,11 @@ $app->post('/signup', 'App\Controllers\UserController:validation_signup');
 $app->get('/signin', 'App\Controllers\UserController:signin')->setName('signin')->add($middleware_need_no_co);
 
 $app->post('/signin', 'App\Controllers\UserController:validation_signin');
+
+$app->get('/homeCo','App\Controllers\UserController:homeCo')->setName('homeCo')->add($middleware_need_co);
+
+$app->get('/disconnect','App\Controllers\UserController:disconnect')->setName('disconnect')->add($middleware_need_co);
+
 
 $app->get('/lists', 'App\Controllers\ListeController:displayLists')->setName('lists');
 
@@ -22,5 +29,6 @@ $app->get('/liste/{id}/generateSharingToken','App\Controllers\ListeController:ge
 //ajouter middleware pour verifier si l'utilisateur qui clique est le createur de la liste//soit rajouter la verification dans la function
 $app->get('/liste/{id}/generateSharingFinalToken','App\Controllers\ListeController:generateSharingFinalToken')->setName('tokenfinal');
 
-$app->get('/homeCo','App\Controllers\UserController:homeCo')->setName('homeCo')->add($middleware_need_co);
-$app->get('/disconnect','App\Controllers\UserController:disconnect')->setName('disconnect')->add($middleware_need_co);
+//creation d'une liste
+$app->get('/creatList', 'App\Controllers\ListeController:creatList')->setName('creatList');
+$app->post('/creatList', 'App\Controllers\ListeController:validation_creatList');
