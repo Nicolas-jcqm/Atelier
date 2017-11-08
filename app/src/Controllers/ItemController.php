b37747fa9246a5c5586c543bd343028929a5d0d9
Orionc <Nicolas.jcqm@gmail.com>
2017-11-08 16:16:33 +0100

    mise en page lists

--- app/src/Controllers/ItemController.php
+++ app/src/Controllers/ItemController.php
@@ -27,14 +27,14 @@ final class ItemController
         $Item->description = $request->getParsedBodyParam("desc");
         $Item->price = $request->getParsedBodyParam("price");
         $Item->url = $request->getParsedBodyParam("url");
-        $Item->idList = $request->getParsedBodyParam("idform")
+        $Item->idList = $request->getParsedBodyParam("idform");
         $Item->idGroup = null;
         
         if (isset($_FILES['FTU']) AND $_FILES['FTU'] ['error'] == 0){
             $nomdate = date('o').'-'.date("m").'-'.date('d').'-'.date('H').'-'.date('i').'-'.date('s');
             $ext = pathinfo($_FILES["FTU"]["name"], PATHINFO_EXTENSION);
             move_uploaded_file($_FILES['FTU'] ['tmp_name'], 'img/' .$nomdate.'.'.$ext);
-            $Item->picture; = $nomdate.'.'.$ext;
+            $Item->picture = $nomdate.'.'.$ext;
             
         }else{
             echo "Sorry, there was an error uploading your file.";
