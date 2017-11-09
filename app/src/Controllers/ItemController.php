e997b774137446b607a0c5645264e9bbe93f18a1
Matmat Myriam <matmat.myriam@gmail.com>
2017-11-09 15:58:48 +0100

    first step to book item

--- app/src/Controllers/ItemController.php
+++ app/src/Controllers/ItemController.php
@@ -10,7 +10,6 @@ use App\Models\Booking;
 use App\Models\Lists;
 use App\Models\Creator;
 use App\Models\Comment;
-use App\Models\Lists;
 
 final class ItemController
 {
@@ -84,8 +83,53 @@ final class ItemController
         $this->view->render($response, 'item.twig', ["creator" =>$creator, "item" =>$item, "url" =>$url, "idlist" =>$args['id'], "formcrea" => $formcrea]);
         
     }
+    public function bookItem(Request $request, Response $response, $args){
+        $parsedBody = $request;
+        $erreurArray=array();
+
+        if(isset($parsedBody) && $parsedBody->getParsedBodyParam('envoi') === "Reserver cette item" ) {
+            $reserverName = $parsedBody->getParsedBodyParam('reserverName');
+            $message = $parsedBody->getParsedBodyParam('message');
+            $idItem= $parsedBody->getParsedBodyParam('iditem');
+
+            if (empty($reserverName)) {
+                $erreurReserverName = "Merci d'entrer un nom";
+                array_push($erreurArray, $erreurReserverName);
+            } else {
+                if ($reserverName != filter_var($reserverName, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH)) {
+                    $erreurReserverNameFiltre = "Merci d'entrer un prénom valide";
+                    array_push($erreurArray, $erreurReserverNameFiltre);
+                }
+            }
+            if(empty($message)){
+                $erreurMessage="Merci d'entrer un nom";
+                array_push($erreurArray,$erreurMessage);
+            }else{
+                if($message != filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH)){
+                    $erreurMessageFiltre ="Merci d'entrer un prénom valide";
+                    array_push($erreurArray,$erreurMessageFiltre);
+                }
+            }
+
+            if (sizeof($erreurArray)===0){
+                $book = new Booking();
+                $book->id = uniqid();
+                $book->reserverName =$reserverName;
+                $book->message=$message;
+                var_dump($idItem);
+                $book->idItem=1;
+                $book->save();
+
+                return $response->withRedirect($this->router->pathFor('viewGuest'));
+
+
+            }
+
+        }
+    }
 
 
 
-    
- }
+
+
+}
